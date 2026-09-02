b4d4a6f1d8b5e3755d38b0df91d7a1b564f8749a
K2976 <sanghikartik2976@gmail.com>
2026-09-02 18:07:14 +0530

    latest

--- To do list/ContentView.swift
+++ To do list/ContentView.swift
@@ -13,69 +13,77 @@ struct ContentView: View{
     @State var itemList:[item] = [item(name: "Study Swift", completed: false, important: false), item(name: "Complete Assignment", completed: false, important: false)]
     @State var ind: Int?
     var body: some View {
-        
-        Text("To Do List")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            .foregroundStyle(.blue)
-        HStack{
-            TextField("Enter New task", text: $task, prompt: Text("Enter the task").foregroundStyle(.black))
-                .frame(width: 300, height: 30)
-                .padding(10)
-                .background(.thinMaterial)
-                .foregroundStyle(.black)
-                .clipShape(.rect(cornerRadius: 30))
+        NavigationStack{
             
-            Button(){
-                addTask()
+            Text("To Do List")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.blue)
+            HStack{
+                TextField("Enter New task", text: $task, prompt: Text("Enter the task").foregroundStyle(.black))
+                    .frame(width: 300, height: 30)
+                    .padding(10)
+                    .background(.thinMaterial)
+                    .foregroundStyle(.black)
+                    .clipShape(.rect(cornerRadius: 30))
+                
+                Button(){
+                    addTask()
+                } label: {
+                    Text("+")
+                        .font(.title2)
+                        .padding(20)
+                        .background(.blue)
+                        .foregroundStyle(.white)
+                        .clipShape(.circle)
+                }
+            }
+            List{
+                ForEach(itemList.indices, id: \.self){ index in
+                    HStack{
+                        //                    Image(systemName: "circle")
+                        Button{
+                            itemList[index].completed.toggle()
+                        } label: {
+                            if(itemList[index].completed){
+                                Image(systemName: "checkmark.circle")
+                            }
+                            else{
+                                Image(systemName: "circle")
+                            }
+                            //                        Image(systemName: itemList[index].completed ? "checkmark.circle":"circle")
+                        }
+                        Text(itemList[index].name)
+                        if(itemList[index].important){
+                            Image(systemName: "star.fill")
+                        }
+                    }
+                    .swipeActions(edge : .leading){
+                        Button{
+                            itemList[index].important.toggle()
+                        } label: {
+                            Text("mark Important")
+                        }
+                    }
+                    .swipeActions(edge: .trailing){
+                        Button{
+                            ind=index
+                            removeTask()
+                        } label:{
+                            Text("Delete")
+                        }
+                        .tint(.red)
+                    }
+                }
+            }
+            .listStyle(.plain)
+            NavigationLink{
+                settingView()
             } label: {
-                Text("+")
-                    .font(.title2)
-                    .padding(20)
-                    .background(.blue)
-                    .foregroundStyle(.white)
-                    .clipShape(.circle)
+                Image(systemName: "gear")
+                
             }
         }
-        List{
-            ForEach(itemList.indices, id: \.self){ index in
-                HStack{
-//                    Image(systemName: "circle")
-                    Button{
-                        itemList[index].completed.toggle()
-                    } label: {
-                        if(itemList[index].completed){
-                            Image(systemName: "checkmark.circle")
-                        }
-                        else{
-                            Image(systemName: "circle")
-                        }
-//                        Image(systemName: itemList[index].completed ? "checkmark.circle":"circle")
-                    }
-                    Text(itemList[index].name)
-                    if(itemList[index].important){
-                        Image(systemName: "star.fill")
-                    }
-                }
-                .swipeActions(edge : .leading){
-                    Button{
-                        itemList[index].important.toggle()
-                    } label: {
-                        Text("mark Important")
-                    }
-                }
-                .swipeActions(edge: .trailing){
-                    Button{
-                        ind=index
-                        removeTask()
-                    } label:{
-                        Text("Delete")
-                    }
-                    .tint(.red)
-                }
-            }
-        }
-        .listStyle(.plain)
     }
     
     func addTask(){
---
+++ To do list/settingView.swift
@@ -0,0 +1,18 @@
+//
+//  settingView.swift
+//  To do list
+//
+//  Created by Kartik on 02-09-2026.
+//
+
+import SwiftUI
+
+struct settingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    settingView()
+}
